5bb82502f787d2a14c4853a0b1775a0bdaab095d
Justin-Lloyd <justin.lloyd@medway.gov.uk>
2019-10-30 16:54:21 +0000

    Changes as requested

--- src/Grok.Net.Tests/UnitTests.cs
+++ src/Grok.Net.Tests/UnitTests.cs
@@ -28,7 +28,7 @@ namespace GrokNetTests
             Grok act = new Grok(grokPattern);
             string logs = @"06-21-19 21:00:13:589241;15;INFO;main;DECODED: 775233900043 DECODED BY: 18500738 DISTANCE: 1.5165
                06-21-19 21:00:13:589265;156;WARN;main;DECODED: 775233900043 EMPTY DISTANCE: --------";
-            int elements = 16;
+            const int elements = 16;
             
             GrokResult grokResult = act.Parse(logs);            
             
@@ -41,7 +41,7 @@ namespace GrokNetTests
             Grok act = new Grok("%{MONTHDAY:month}-%{MONTHDAY:day}-%{MONTHDAY:year} %{TIME:timestamp};%{WORD:id};%{LOGLEVEL:loglevel};%{WORD:func};%{GREEDYDATA:msg}");
             string logs = @"06-21-19 21:00:13:589241;15;INFO;main;DECODED: 775233900043 DECODED BY: 18500738 DISTANCE: 1.5165
                06-21-19 21:00:13:589265;156;WARN;main;DECODED: 775233900043 EMPTY DISTANCE: --------";
-            const string month="06";
+            const string month = "06";
             const string day = "21";
             const string year = "19";   
             
@@ -90,35 +90,19 @@ namespace GrokNetTests
             Assert.Equal("comment", grokResult[1].Key);
         }            
 
-        [Theory]
-        [MemberData(nameof(GetIpv4PatternTestData))]
-        public void IPv4PatternTest(string grokPattern, string logs, string ipAddress, string comment, int grokElementOffset)
+        [Fact]
+        public void IPv4PatternTest()
         {
+            const string ipAddress = "172.26.34.32";
+            const string comment = "Free as in Free Beer"; 
+            string logs = $"{ipAddress}:{comment}";
+            string grokPattern="%{IPV4:IP}:%{GREEDYDATA:comment}";
             Grok act = new Grok(grokPattern);
             
             GrokResult grokResult = act.Parse(logs);
            
-            Assert.Equal(ipAddress, grokResult[grokElementOffset].Value);
-            Assert.Equal(comment, grokResult[grokElementOffset+1].Value);
+            Assert.Equal(ipAddress, grokResult[0].Value);
+            Assert.Equal(comment, grokResult[1].Value);
         }  
-        public static IEnumerable<object[]> GetIpv4PatternTestData()
-        { 
-            string grokPattern="%{IPV4:IP}:%{GREEDYDATA:comment}";
-            const string ip1 ="172.26.34.32";
-            const string comment1 = "Free as in Free Beer";
-            const string ip2="10.0.12.17";
-            const string comment2="In for the win";
-            string logs = ip1 + ":" + comment1 +"\n"+ ip2 + ":" + comment2;
-            int elementOffset = 0;    
-
-            var data = new List<object[]>
-            {
-                new object[]{grokPattern, logs, ip1, comment1, elementOffset},
-                new object[]{grokPattern, logs, ip2, comment2, elementOffset+2},
-            };
-
-            return data;            
-        }
-
     }
 }
