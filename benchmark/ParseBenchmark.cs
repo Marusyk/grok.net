a7c8b4ce2733f95dcfba9d7de6db183509f83079
halilkocaoz <halil.i.kocaoz@gmail.com>
2021-08-04 01:46:28 +0300

    Update variable names & README.MD. Add benchmark project to sln. Move sln file.

--- benchmark/ParseBenchmark.cs
+++ benchmark/ParseBenchmark.cs
@@ -5,26 +5,26 @@ namespace Benchmark
 {
     public class ParseBenchmark
     {
-        private static readonly Grok grokEmpty = new Grok("");
-        private static readonly Grok grokLog = new Grok("%{MONTHDAY:month}-%{MONTHDAY:day}-%{MONTHDAY:year} %{TIME:timestamp};%{WORD:id};%{LOGLEVEL:loglevel};%{WORD:func};%{GREEDYDATA:msg}");
-        private static readonly Grok grokCustom = new Grok("%{ZIPCODE:zipcode}:%{EMAILADDRESS:email}");
+        private static readonly Grok _grokEmpty = new Grok("");
+        private static readonly Grok _grokLog = new Grok("%{MONTHDAY:month}-%{MONTHDAY:day}-%{MONTHDAY:year} %{TIME:timestamp};%{WORD:id};%{LOGLEVEL:loglevel};%{WORD:func};%{GREEDYDATA:msg}");
+        private static readonly Grok _grokCustom = new Grok("%{ZIPCODE:zipcode}:%{EMAILADDRESS:email}");
 
         [Benchmark]
         public void Empty()
         {
-            _ = grokEmpty.Parse("");
+            _ = _grokEmpty.Parse("");
         }
 
         [Benchmark]
         public void Custom()
         {
-            _ = grokCustom.Parse("06590:halil.i.kocaoz@gmail.com");
+            _ = _grokCustom.Parse("06590:halil.i.kocaoz@gmail.com");
         }
 
         [Benchmark]
         public void Log()
         {
-            _ = grokLog.Parse(@"06-21-19 21:00:13:589241;15;INFO;main;DECODED: 775233900043 DECODED BY: 18500738 DISTANCE: 1.5165
+            _ = _grokLog.Parse(@"06-21-19 21:00:13:589241;15;INFO;main;DECODED: 775233900043 DECODED BY: 18500738 DISTANCE: 1.5165
                06-21-19 21:00:13:589265;156;WARN;main;DECODED: 775233900043 EMPTY DISTANCE: --------");
         }
 
