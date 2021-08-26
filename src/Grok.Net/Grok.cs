f87274450704f0445a293d05e9c541c9e09d3bc7
Giovanni Maria Zanchetta <giovanni@Giovannis-Air.homenet.telecomitalia.it>
2021-08-26 21:57:30 +0200

    moved pattern validation inside ProcessPatternLine

--- src/Grok.Net/Grok.cs
+++ src/Grok.Net/Grok.cs
@@ -144,17 +144,7 @@ namespace GrokNet
                 {
                     while (!sr.EndOfStream)
                     {
-                        string line = sr.ReadLine();
-                        string[] pattern = line.Split(' ');
-                        try
-                        {
-                            Regex.Match("", pattern[1]);
-                            ProcessPatternLine(line);
-                        }
-                        catch (ArgumentException e)
-                        {
-                            Console.WriteLine(e.Message);
-                        }
+                        ProcessPatternLine(sr.ReadLine());
                     }
                 }
             }
@@ -168,7 +158,20 @@ namespace GrokNet
             }
 
             string[] strArray = line.Split(new[] { ' ' }, 2);
+            if (strArray.Length != 2)
+            {
+                return;
+            }
             if (strArray[0].Equals("#", StringComparison.OrdinalIgnoreCase))
+            {
+                return;
+            }
+
+            try
+            {
+                Regex.Match("", strArray[1]);
+            }
+            catch
             {
                 return;
             }
