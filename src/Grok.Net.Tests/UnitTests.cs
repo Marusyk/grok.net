0f8ab865a4f9e5e26602fd8e44631388c77907ef
Nikhil Patil <nikhil.patil.3632@gmail.com>
2023-02-24 20:46:46 +0530

    Added pattern for base64 string

--- src/Grok.Net.Tests/UnitTests.cs
+++ src/Grok.Net.Tests/UnitTests.cs
@@ -283,5 +283,25 @@ namespace GrokNetTests
             // Act, Assert
             Assert.Throws<ArgumentNullException>(() => sut.Parse(logs));
         }
+
+        [Theory]
+        [InlineData("dGVzdCBzdHJpbmc=")]
+        [InlineData("")]
+        [InlineData("VGhpcyBpcyBhIHNpbXBsZSBBU0NJSSBCYXNlNjQgZXhhbXBsZSBmb3IgU3RhY2tPdmVyZmxvdy4=")]
+        [InlineData("TG9yZW0gSXBzdW0gaXMgc2ltcGx5IGR1bW15IHRleHQgb2YgdGhlIHByaW50aW5nIGFuZCB0eXBlc2V0dGluZyBpbmR1c3RyeS4gTG9yZW0gSXBzdW0gaGFzIGJlZW4gdGhlIGluZHVzdHJ5J3Mgc3RhbmRhcmQgZHVtbXkgdGV4dCBldmVyIHNpbmNlIHRoZSAxNTAwcywgd2hlbiBhbiB1bmtub3duIHByaW50ZXIgdG9vayBhIGdhbGxleSBvZiB0eXBlIGFuZCBzY3JhbWJsZWQgaXQgdG8gbWFrZSBhIHR5cGUgc3BlY2ltZW4gYm9vay4gSXQgaGFzIHN1cnZpdmVkIG5vdCBvbmx5IGZpdmUgY2VudHVyaWVzLCBidXQgYWxzbyB0aGUgbGVhcCBpbnRvIGVsZWN0cm9uaWMgdHlwZXNldHRpbmcsIHJlbWFpbmluZyBlc3NlbnRpYWxseSB1bmNoYW5nZWQuIEl0IHdhcyBwb3B1bGFyaXNlZCBpbiB0aGUgMTk2MHMgd2l0aCB0aGUgcmVsZWFzZSBvZiBMZXRyYXNldCBzaGVldHMgY29udGFpbmluZyBMb3JlbSBJcHN1bSBwYXNzYWdlcywgYW5kIG1vcmUgcmVjZW50bHkgd2l0aCBkZXNrdG9wIHB1Ymxpc2hpbmcgc29mdHdhcmUgbGlrZSBBbGR1cyBQYWdlTWFrZXIgaW5jbHVkaW5nIHZlcnNpb25zIG9mIExvcmVtIElwc3Vt")]
+        public void Parse_Base64String_Patterns(string base64String)
+        {
+            // Arrange
+            var sut = new Grok("%{BASE64STR:base64}");
+
+            // Act
+            GrokResult grokResult = sut.Parse(base64String);
+
+            // Assert
+            Assert.Equal("base64", grokResult[0].Key);
+            Assert.IsType<string>(grokResult[0].Value);
+            Assert.Equal(base64String, grokResult[0].Value);
+            Assert.Single(grokResult);
+        }
     }
 }
