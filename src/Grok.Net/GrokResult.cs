435d30bf054c356307749ee047421892bd8f546d
Giovanni Maria Zanchetta <48056684+GioMaz@users.noreply.github.com>
2023-08-22 01:00:38 +0200

    Get Dictionary instead of GrokResult with AsDictionary method (#72) (#75)
    Add ToDictionary as GrokResult method

--- src/Grok.Net/GrokResult.cs
+++ src/Grok.Net/GrokResult.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace GrokNet
 {
@@ -9,5 +10,9 @@ namespace GrokNet
             : base(grokItems ?? new List<GrokItem>())
         {
         }
+
+        public IReadOnlyDictionary<string, IEnumerable<object>> ToDictionary() => Items
+            .GroupBy(grokItem => grokItem.Key)
+            .ToDictionary(groupName => groupName.Key, grokItems => grokItems.Select(grokItem => grokItem.Value));
     }
 }
